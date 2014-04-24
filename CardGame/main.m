082daa7e45bacca874d9d02ff5d6ec607b149eca
grivero <grivero@altimetrik.com>
2014-04-24 15:06:27 -0700

    # initial commit - core stuff from the project

---
+++ CardGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CardGame
+//
+//  Created by grivero on 4/21/14.
+//  Copyright (c) 2014 grivero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
