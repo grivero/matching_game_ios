8940cd2b856a698891ee10591400806b80287154
grivero <grivero@altimetrik.com>
2014-04-24 15:07:58 -0700

    - delegates (no need to know about them yet)

---
+++ CardGame/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  CardGame
+//
+//  Created by grivero on 4/21/14.
+//  Copyright (c) 2014 grivero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
